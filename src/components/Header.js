b1c45dcf87787f28b8e182dce6a1feb8b637bd67
Mai <garcia.ton94@gmail.com>
2023-07-17 14:48:47 -0400

    edited nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,6 +20,11 @@ export default function Header({ user, setUser }) {
         console.log(error);
       });
   }
+
+  function handleClick() {
+    handleLogoutClick();
+    setIsOpenMenu(false);
+  }
   return (
     <>
       <header className="main-header fixed left-0 right-0 z-[111]">
@@ -30,7 +35,7 @@ export default function Header({ user, setUser }) {
             </NavLink>
           </div>
           <button
-            className="lg:hidden mobile_toggle w-[40px] flex flex-col"
+            className="lg:hidden mobile_toggle w-[40px] flex flex-col bg-transparent"
             onClick={() => setIsOpenMenu(!isOpenMenu)}
           >
             <span className="w-[25px] h-[2px] bg-slate-900 inline-block"></span>
@@ -42,44 +47,184 @@ export default function Header({ user, setUser }) {
           >
             <ul className="navbar lg:mx-auto flex flex-col lg:flex-row p-4 lg:p-0">
               <li>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to="/" onClick={() => setIsOpenMenu(false)}>
+                  Home
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/pets">Pets</NavLink>
+                <NavLink to="/pets" onClick={() => setIsOpenMenu(false)}>
+                  Pets
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/meetups">Meetups</NavLink>
+                <NavLink to="/meetups" onClick={() => setIsOpenMenu(false)}>
+                  Meetups
+                </NavLink>
               </li>
-            </ul>
-          </div>
-          <>
-            {user && (
-              <>
-                <div className="ms-auto hidden lg:flex pr-[10px]">
-                  <NavLink className="px-btn px-btn-theme" to="/dashboard">
+              {isOpenMenu && !user && (
+                <div className="lg:flex pr-[10px]">
+                  <NavLink
+                    className="px-btn px-btn-dark"
+                    to="login"
+                    onClick={() => setIsOpenMenu(false)}
+                  >
+                    Login
+                  </NavLink>
+                </div>
+              )}
+              {isOpenMenu && !user && (
+                <div className="pt-2 lg:flex pr-[10px]">
+                  <NavLink
+                    className="px-btn px-btn-theme"
+                    to="signup"
+                    onClick={() => setIsOpenMenu(false)}
+                  >
+                    Signup
+                  </NavLink>
+                </div>
+              )}
+              {isOpenMenu && user && (
+                <div className="lg:flex pr-[10px]">
+                  <NavLink
+                    className="px-btn px-btn-theme"
+                    to="/dashboard"
+                    onClick={() => setIsOpenMenu(false)}
+                  >
                     Dashboard
                   </NavLink>
                 </div>
-                <div className="ms-auto hidden lg:flex">
-                  <NavLink
-                    className="px-btn px-btn-dark"
-                    onClick={handleLogoutClick}
-                  >
+              )}
+              {isOpenMenu && user && (
+                <div className="flex flex-row items-center space-x-2 mt-4">
+                  <NavLink className="relative inline-block " to="/dashboard">
+                    <img className="nav-avatar" src={user.profile_pic} />
+                  </NavLink>
+                  <NavLink className="pl-2" onClick={handleClick}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-box-arrow-right nav-icon"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                      />
+                    </svg>
                     Logout
                   </NavLink>
                 </div>
-              </>
-            )}
-          </>
+              )}
+            </ul>
+          </div>
+
+          {user && (
+            <>
+              <div className="ms-auto hidden lg:flex pr-[5px]">
+                <NavLink
+                  to="/dashboard"
+                  className="flex flex-col items-center spacy-y-1.5 relative text-xs mx-3"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-gear nav-icon"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+                    <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+                  </svg>
+                  <p>Dashboard</p>
+                </NavLink>
+              </div>
+
+              <div className="ms-auto hidden lg:flex pr-[15px]">
+                <NavLink
+                  onClick={handleLogoutClick}
+                  className="flex flex-col items-center spacy-y-1.5 relative text-xs"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-box-arrow-right nav-icon"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                    />
+                  </svg>
+
+                  <p>Logout</p>
+                </NavLink>
+              </div>
+              <div className="ms-auto hidden lg:flex">
+                <NavLink className="relative inline-block" to="/dashboard">
+                  <img className="nav-avatar" src={user.profile_pic} />
+                </NavLink>
+              </div>
+            </>
+          )}
           {!user && (
             <>
               <div className="ms-auto hidden lg:flex pr-[10px]">
-                <NavLink className="px-btn px-btn-dark" to="/login">
+                <NavLink
+                  className="flex flex-col items-center spacy-y-1.5 relative text-xs"
+                  to="/login"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-box-arrow-in-right nav-icon"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                    />
+                  </svg>
                   Login
                 </NavLink>
               </div>
               <div className="ms-auto hidden lg:flex">
-                <NavLink className="px-btn px-btn-theme" to="/signup">
+                <NavLink
+                  className="flex flex-col items-center spacy-y-1.5 relative text-xs"
+                  to="/signup"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-person-plus nav-icon"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                    />
+                  </svg>
                   Signup
                 </NavLink>
               </div>
