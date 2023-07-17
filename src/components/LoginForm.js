beb126e56a9a989a11c9e5602b50ad4083ab9a9f
Mai <garcia.ton94@gmail.com>
2023-07-17 12:34:24 -0400

    some updates

---
+++ src/components/LoginForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginForm({ onLogin }) {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => {
+          onLogin(user);
+          navigate("/"); // Redirect to the homepage
+        });
+      } else {
+        r.json().then((err) => setErrorMessage(err.error));
+      }
+    });
+  }
+
+  return (
+    <div className="container mx-auto form-container">
+      <form onSubmit={handleSubmit} className="form">
+        <div className="mb-5">
+          <label htmlFor="username" className="form-instruction">
+            Enter username and password to login
+          </label>
+        </div>
+        <div>
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="form-control"
+            autoComplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" className="px-btn px-btn-theme mt-4">
+            {isLoading ? "Loading..." : "Login"}
+          </button>
+        </div>
+        <div>{errorMessage && <div className="error">{errorMessage}</div>}</div>
+      </form>
+    </div>
+  );
+}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import PageTransition from "./PageTransition";
 
 export default function Signup({ onLogin }) {
   const navigate = useNavigate();
@@ -44,83 +45,90 @@ export default function Signup({ onLogin }) {
   }
 
   return (
-    <section className="pt-[120px] pb-[80px] lg:pt-[180px] lg:pb-[100px] relative overflow-hidden">
-      <div className="container mx-auto">
-        <form onSubmit={handleSubmit} className="form">
-          <div>
-            <label htmlFor="username" className="form-label">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              className="form-control"
-              autoComplete="off"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="form-control"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              autoComplete="current-password"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="form-label">
-              Password Confirmation
-            </label>
-            <input
-              type="password"
-              id="password_confirmation"
-              className="form-control"
-              value={passwordConfirmation}
-              onChange={(e) => setPasswordConfirmation(e.target.value)}
-              autoComplete="current-password"
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className="form-label">
-              Email Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="form-control"
-              autoComplete="off"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="profile_pic" className="form-label">
-              Profile Image
-            </label>
-            <input
-              type="text"
-              id="profile_pic"
-              className="form-control"
-              value={profile_pic}
-              onChange={(e) => setProfilePic(e.target.value)}
-            />
-          </div>
-          <div>
-            <button type="submit" className="px-btn px-btn-theme mt-4">
-              {isLoading ? "Loading..." : "Sign Up"}
-            </button>
-          </div>
-          <div>
-            {errorMessage && <div className="error">{errorMessage}</div>}
-          </div>
-        </form>
-      </div>
-    </section>
+    <PageTransition>
+      <section className="pt-[120px] pb-[80px] lg:pt-[170px] lg:pb-[100px] bg-blue relative overflow-hidden h-screen">
+        <div className="container mx-auto">
+          <form onSubmit={handleSubmit} className="form">
+            <div className="mb-5">
+              <label htmlFor="username" className="form-instruction">
+                Fill out your information to sign up
+              </label>
+            </div>
+            <div>
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="form-control"
+                autoComplete="off"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password Confirmation
+              </label>
+              <input
+                type="password"
+                id="password_confirmation"
+                className="form-control"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+                autoComplete="current-password"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="form-label">
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                autoComplete="off"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="profile_pic" className="form-label">
+                Profile Image
+              </label>
+              <input
+                type="text"
+                id="profile_pic"
+                className="form-control"
+                value={profile_pic}
+                onChange={(e) => setProfilePic(e.target.value)}
+              />
+            </div>
+            <div>
+              <button type="submit" className="px-btn px-btn-theme mt-4">
+                {isLoading ? "Loading..." : "Sign Up"}
+              </button>
+            </div>
+            <div>
+              {errorMessage && <div className="error">{errorMessage}</div>}
+            </div>
+          </form>
+        </div>
+      </section>
+    </PageTransition>
   );
 }
