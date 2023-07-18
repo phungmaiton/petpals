f9090adee3d4d3d527659446687ee7c8148e94d1
Katherine West <katherinewest98@yahoo.com>
2023-07-18 06:29:45 -0500

    Details

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -32,7 +32,7 @@ export default function Dashboard({ user }) {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            className="bi bi-pencil-square dashboard-icon"
+                            className="bi bi-pencil-square dashboard-icon cursor-pointer"
                             viewBox="0 0 16 16"
                           >
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -65,7 +65,7 @@ export default function Dashboard({ user }) {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            className="bi bi-plus-square dashboard-icon"
+                            className="bi bi-plus-square dashboard-icon cursor-pointer"
                             viewBox="0 0 16 16"
                           >
                             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
@@ -79,7 +79,7 @@ export default function Dashboard({ user }) {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            className="bi bi-x-square dashboard-icon"
+                            className="bi bi-x-square dashboard-icon cursor-pointer"
                             viewBox="0 0 16 16"
                           >
                             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
@@ -93,7 +93,7 @@ export default function Dashboard({ user }) {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            className="bi bi-pencil-square dashboard-icon"
+                            className="bi bi-pencil-square dashboard-icon cursor-pointer"
                             viewBox="0 0 16 16"
                           >
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -132,7 +132,7 @@ export default function Dashboard({ user }) {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            className="bi bi-plus-square dashboard-icon"
+                            className="bi bi-plus-square dashboard-icon cursor-pointer"
                             viewBox="0 0 16 16"
                           >
                             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
@@ -146,7 +146,7 @@ export default function Dashboard({ user }) {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            className="bi bi-x-square dashboard-icon"
+                            className="bi bi-x-square dashboard-icon cursor-pointer"
                             viewBox="0 0 16 16"
                           >
                             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
@@ -160,7 +160,7 @@ export default function Dashboard({ user }) {
                             width="16"
                             height="16"
                             fill="currentColor"
-                            className="bi bi-pencil-square dashboard-icon"
+                            className="bi bi-pencil-square dashboard-icon cursor-pointer"
                             viewBox="0 0 16 16"
                           >
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
